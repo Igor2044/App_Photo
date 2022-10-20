69c5df9c7ad2a182d4243061294e4c20cdcb7d75
Dominos0321 <dsch792@gmail.com>
2022-10-20 08:19:19 +0200

    nie działa nwm

--- app/src/main/java/com/example/app_photo/MainActivity.kt
+++ app/src/main/java/com/example/app_photo/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.app_photo
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.core.content.ContextCompat
+import androidx.viewbinding.ViewBinding
+import com.example.app_photo.databinding.ActivityMainBinding
 import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ResultProfileBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ResultProfileBinding.inflate(layoutInflater)
-        val view = bindig.root
-        setContentView(view)
+        binding = ActivityMainBinding.inflate(LayoutInflater)
+        setContentView(binding.root)
     }
 
 }
