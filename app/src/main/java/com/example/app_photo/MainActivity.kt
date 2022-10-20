7ca910b407960ea0fe049d19b2c3ce8901ddd242
Dominos0321 <dsch792@gmail.com>
2022-10-20 09:02:15 +0200

    robimy inaczej

--- app/src/main/java/com/example/app_photo/MainActivity.kt
+++ app/src/main/java/com/example/app_photo/MainActivity.kt
@@ -4,20 +4,17 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.viewbinding.ViewBinding
-import com.example.app_photo.databinding.ActivityMainBinding
 import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater)
-        setContentView(binding.root)
+        val view = setContentView(this,R.layout.activity_main)
+
     }
 
 }
