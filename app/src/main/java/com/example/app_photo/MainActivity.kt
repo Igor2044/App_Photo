c3008ee21fb62d1ae446159fd2606034ba8f2081
Dominos0321 <dsch792@gmail.com>
2022-10-20 09:20:52 +0200

    zmiany zamiast binding i kamera

--- app/src/main/java/com/example/app_photo/MainActivity.kt
+++ app/src/main/java/com/example/app_photo/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.app_photo
 
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.jar.Manifest
 
@@ -13,8 +23,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = setContentView(this,R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
+        val Obrazek: ImageView =findViewById(R.id.imgPhoto)
+        val BtnCamera: Button =findViewById(R.id.btnPhoto)
+
+        BtnCamera.isEnabled=false
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA )!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),111)
+        }else{
+            BtnCamera.isEnabled=true
+        }
+        BtnCamera.setOnClickListener(){
+            var x = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(x,101)
+        }
     }
-
 }
